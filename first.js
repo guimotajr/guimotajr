efa4c1d3f81cb1b7ad96ebd8be69dfc41c3c0a16
Guilherme Mota <guilherme@ubuntu.(none)>
2013-07-03 11:37:30 -0300

    teste upload

--- first.js
+++ first.js
@@ -3,4 +3,4 @@ var fs = require('fs');
 var outfile = "hello.txt";
 var out = "A startup is a business built to grow rapidly, gui.\n";
 fs.writeFileSync(outfile, out);
-console.log("Script: " + fristApp + "\nWrote: "+ out + "To: " + outfile);
+console.log("Script: " + firstApp + "\nWrote: "+ out + "To: " + outfile);
