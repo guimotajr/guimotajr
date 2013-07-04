4c63d393b380aad16145f7ca706c481ae1ca3b3d
Guilherme Mota <guilherme@ubuntu.(none)>
2013-07-03 23:23:44 -0300

    teste2

--- first.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-var fs = require('fs');
-var outfile = "hello.txt";
-var out = "A startup is a business built to grow rapidly, gui.\n";
-fs.writeFileSync(outfile, out);
-console.log("Script: " + firstApp + "\nWrote: "+ out + "To: " + outfile);
